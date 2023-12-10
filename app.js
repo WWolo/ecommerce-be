1792b4fe8093b2ecf6fa545353dbbad6b2a4907d
WojtekWoloszyn <wojtekwoloszyn00@gmail.com>
2023-12-10 18:10:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+
+require('dotenv/config')
+
+const api = process.env.API_URL
+
+app.use(cors())
+app.options('*', cors())
+
+// middleware
+app.use(express.json())
+app.use(morgan('tiny'))
+
+// router
+const categoriesRouter = require('./routers/categories')
+const ordersRouter = require('./routers/orders')
+const productsRouter = require('./routers/products')
+const usersRouter = require('./routers/users')
+
+app.use(`${api}/categories`, categoriesRouter)
+app.use(`${api}/orders`, ordersRouter)
+app.use(`${api}/products`, productsRouter)
+app.use(`${api}/users`, usersRouter)
+
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING, {
+    dbName: 'eshop-database',
+  })
+  .then(() => {
+    console.log('Database Connection is ready...')
+  })
+  .catch((err) => {
+    console.log('err :>> ', err)
+  })
+
+app.listen(3000, () => {
+  console.log(api)
+  console.log(`listening on http://localhost:3000`)
+})
---
+++ routers/orders.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { Order } = require('../models/order')
+
+const router = express.Router()
+
+router.get(`/`, async (req, res) => {
+  const orderList = await Order.find()
+
+  if (!orderList) {
+    res.status(500).json({ success: false })
+  }
+
+  res.send(orderList)
+})
+
+module.exports = router
