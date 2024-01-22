81ad9518f67a4015b1f3ca4a7f267a5290f6187f
WojtekWoloszyn <wojtekwoloszyn00@gmail.com>
2024-01-22 20:35:42 +0100

    users and authenticate

---
+++ helpers/jwt.js
@@ -0,0 +1,28 @@
+const expressJwt = require('express-jwt')
+
+async function isRevoked(req, payload, done) {
+  if (!payload.isAdmin) {
+    done(null, true)
+  }
+  done()
+}
+
+function authJwt() {
+  const secret = process.env.SECRET
+  const api = process.env.API_URL
+
+  return expressJwt({
+    secret,
+    algorithms: ['HS256'],
+    isRevoked,
+  }).unless({
+    path: [
+      `${api}/users/login`,
+      `${api}/users/register`,
+      { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+    ],
+  })
+}
+
+module.exports = authJwt
--- routers/users.js
+++ routers/users.js
@@ -101,10 +101,33 @@ router.post('/login', async (req, res) => {
       { expiresIn: '1d' }
     )
 
-    return res.status(200).send({ user: user.email, token: token })
-  } 
-    return res.status(400).send('password is wrong!')
-  
+    return res.status(200).send({ user: user.email, token })
+  }
+  return res.status(400).send('password is wrong!')
+})
+
+router.get(`/get/count`, async (req, res) => {
+  const userCount = await User.countDocuments((count) => count)
+
+  if (!userCount) {
+    res.status(500).json({ success: false })
+  }
+
+  res.send({ count: userCount }),
+})
+
+router.delete('/:id', (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+    .then((user) => {
+      if (user) {
+        return res
+          .status(200)
+          .json({ success: true, message: 'The user is delated' })
+      }
+
+      return res.status(404).json({ success: false, message: 'User not found' })
+    })
+    .catch((err) => res.status(400).json({ success: false, error: err }))
 })
 
 module.exports = router
