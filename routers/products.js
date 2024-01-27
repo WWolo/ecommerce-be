e4d675ad455be3c22cf6820ffbdd74c2ad4ccb90
WojtekWoloszyn <wojtekwoloszyn00@gmail.com>
2024-01-27 17:55:17 +0100

    started upload file

--- routers/products.js
+++ routers/products.js
@@ -1,9 +1,37 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const multer = require('multer')
 const { Product } = require('../models/product')
 const { Category } = require('../models/category')
 
 const router = express.Router()
+
+const FILE_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+}
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    const isValid = FILE_TYPE_MAP[file.mimetype]
+    let uploadError = new Error('invalid image type')
+
+    if (isValid) {
+      uploadError = null
+    }
+
+    cb(uploadError, '/public/uploads')
+  },
+  filename(req, file, cb) {
+    const fileName = file.originalname.split(' ').join('-')
+    const extension = FILE_TYPE_MAP[file.mimetype]
+
+    cb(null, `${fileName}-${Date.now()}.${extension}`)
+  },
+})
+
+const uploadOptions = multer({ storage })
 
 router.get(`/`, async (req, res) => {
   let filter = {}
@@ -30,19 +58,21 @@ router.get(`/:id`, async (req, res) => {
   res.send(product)
 })
 
-router.post(`/`, async (req, res) => {
+router.post(`/`, uploadOptions.single('image'), async (req, res) => {
   const category = await Category.findById(req.body.category)
 
   if (!category) {
     return res.status(400).send('Invalid Category')
   }
 
+  const fileName = req.file.filename
+  const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`
+
   let product = new Product({
     name: req.body.name,
     description: req.body.description,
     richDescription: req.body.richDescription,
-    image: req.body.image,
-    images: req.body.images,
+    image: `${basePath}${fileName}`,
     brand: req.body.brand,
     price: req.body.price,
     category: req.body.category,
@@ -102,22 +132,18 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', (req, res) => {
   Product.findByIdAndRemove(req.params.id)
-    .then((product) => {
+    .then(product => {
       if (product) {
-        return res
-          .status(200)
-          .json({ success: true, message: 'The product is delated' })
+        return res.status(200).json({ success: true, message: 'The product is delated' })
       }
 
-      return res
-        .status(404)
-        .json({ success: false, message: 'Product not found' })
+      return res.status(404).json({ success: false, message: 'Product not found' })
     })
-    .catch((err) => res.status(400).json({ success: false, error: err }))
+    .catch(err => res.status(400).json({ success: false, error: err }))
 })
 
 router.get(`/get/count`, async (req, res) => {
-  const productCount = await Product.countDocuments((count) => count)
+  const productCount = await Product.countDocuments(count => count)
 
   if (!productCount) {
     res.status(500).json({ success: false })
